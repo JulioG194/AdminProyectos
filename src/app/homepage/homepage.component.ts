136458e225aed9753d88e2874bb61598f397490b
Julio <julio.gonzalez2014a@gmail.com>
2019-11-13 15:43:52 -0500

    Primera version 2

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -3,13 +3,19 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
-  styles: []
+  styleUrls: ['./homepage.component.css']
 })
 export class HomepageComponent implements OnInit {
 
   constructor( private router: Router ) { }
 
   ngOnInit() {
+    if (!localStorage.getItem('foo')) {
+      localStorage.setItem('foo', 'no reload');
+      location.reload();
+    } else {
+      localStorage.removeItem('foo');
+    }
   }
 
   public ProbarlaApp() {
